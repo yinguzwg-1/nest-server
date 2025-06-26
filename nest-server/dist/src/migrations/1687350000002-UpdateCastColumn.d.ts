b5e8a71c9bd0618abad00f0586fcea59198697b9
candace <candace-fe@gate.me>
2025-06-26 20:29:22 +0800

    fix: 服务端部署

--- nest-server/dist/src/migrations/1687350000002-UpdateCastColumn.d.ts
+++
@@ -1,6 +0,0 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
-export declare class UpdateCastColumn1687350000002 implements MigrationInterface {
-    name: string;
-    up(queryRunner: QueryRunner): Promise<void>;
-    down(queryRunner: QueryRunner): Promise<void>;
-}
