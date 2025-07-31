6d488d7851befe7eac03ecf7b4fd6cdbfe1bb264
candace <candace-fe@gate.me>
2025-07-31 09:57:03 +0800

    fix: restart

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -23,7 +23,9 @@ module.exports = {
       error_file: './logs/err-1.log',
       out_file: './logs/out-1.log',
       log_file: './logs/combined-1.log',
-      time: true
+      time: true,
+      autorestart: false,
+      max_restarts: 3
     }
   ]
 }; 
